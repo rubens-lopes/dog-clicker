7b8bf84574647659611aa0cd5a707899c1dd2412
Rubens Armôa Lopes <rubens.lopes@gmail.com>
2016-12-19 17:51:31 -0300

    Enhance click event handler

--- js/app.js
+++ js/app.js
@@ -7,58 +7,46 @@
             new Dog('Formal Bilbo', 'images/dog-4.png'),
             new Dog('Rabbit Bilbo', 'images/dog-5.png'),
         ],
-        selectedDog: null,
+        currentDog: null,
     };
 
     var octupus = {
         init: function () {
             listView.init();
-            listView.render();
-
             dogView.init();
-            dogView.render();
         },
         getDogs: function () {
             return model.dogList;
         },
-        setDog: function (e) {
-            var listItem = e.target,
-                selectedDog = model.dogList[listItem.getAttribute('data-dog')],
-                isSame = model.selectedDog === selectedDog;
-            model.selectedDog = !isSame ? selectedDog : null;
-
-            var items = document.getElementsByClassName('dog-list__item--selected');
-            for (var i = 0; i < items.length; i++) {
-                var d = items[i];
-                d.classList.remove('dog-list__item--selected');
-                d.tabIndex = 0;
-            }
-
-            if (!isSame) {
-                listItem.classList.add('dog-list__item--selected');
-                listItem.tabIndex = -1;
-            }
-
+        setDog: function (dog) {
+            var isSame = model.currentDog === dog;
+            model.currentDog = !isSame ? dog : null;
             dogView.render();
         },
-        selectedDog: function () {
-            return model.selectedDog;
+        getCurrentDog: function () {
+            return model.currentDog;
+        },
+        incrementCounter: function () {
+            model.currentDog.clickCount++;
+            dogView.render();
         }
     };
 
     var listView = {
         init: function () {
             this.dogListElement = document.getElementsByClassName('dog-list')[0];
+
+            this.render();
         },
         render: function () {
             var dogListElement = this.dogListElement;
             dogListElement.innerHTML = '';
 
-            octupus.getDogs().forEach(function (e, i) {
+            octupus.getDogs().forEach(function (dog, i) {
                 var dogListItem = document.createElement('li');
 
                 dogListItem.classList.add('dog-list__item');
-                dogListItem.innerText = e.name;
+                dogListItem.innerText = dog.name;
                 dogListItem.tabIndex = 0;
                 dogListItem.setAttribute('data-dog', i);
                 dogListItem.setAttribute('aria-controls', 'dogView');
@@ -69,13 +57,30 @@
                 });
                 dogListItem.addEventListener('keyup', function (e) {
                     if (e.keyCode === 32)
-                        octupus.setDog(e);
+                        e.target.click();
                 });
-                dogListItem.addEventListener('click', function (e) {
-                    octupus.setDog(e);
-                });
+                dogListItem.addEventListener('click', (function (dogCopy) {
+                    return function () {
+                        octupus.setDog(dogCopy);
+                        setAsCurrent(dogListItem);
+                    };
+                })(dog));
 
                 dogListElement.appendChild(dogListItem);
+
+                var setAsCurrent = function (item) {
+                        var items = document.getElementsByClassName('dog-list__item--selected');
+                        for (var i = 0; i < items.length; i++) {
+                            var d = items[i];
+                            d.classList.remove('dog-list__item--selected');
+                            d.tabIndex = 0;
+                        }
+                        
+                        if (octupus.getCurrentDog() !== null) {
+                            item.classList.add('dog-list__item--selected');
+                            item.tabIndex = -1;
+                        }  
+                };
             });
         }
     };
@@ -85,41 +90,39 @@
             this.dogName = this.dog.getElementsByClassName('dog__name')[0];
             this.dogImage = this.dog.getElementsByClassName('dog__image')[0];
             this.dogClickCount = this.dog.getElementsByClassName('dog__click-count')[0];
-            this.eventListener;
             this.dogLoading = this.dog.getElementsByClassName('dog__loading')[0];
+
+            this.dogImage.addEventListener('click', function () {
+                octupus.incrementCounter();
+            });
+
+            this.render();
         },
         render: function () {
             var dogImage = this.dogImage,
                 dogLoading = this.dogLoading;
-            
-            dogImage.removeEventListener('click', this.eventListener);
+
             this.dog.setAttribute('aria-hidden', true);
             this.dog.removeAttribute('aria-expanded');
             dogImage.setAttribute('aria-hidden', true);
             dogLoading.removeAttribute('aria-hidden');
 
-            var selectedDog = octupus.selectedDog();
-            if (selectedDog === null)
+            var currentDog = octupus.getCurrentDog();
+            if (currentDog === null)
                 return;
 
             this.dog.removeAttribute('aria-hidden');
             this.dog.setAttribute('aria-expanded', true);
 
-            this.dogName.innerText = selectedDog.name;
-            dogImage.src = selectedDog.url;
+            this.dogName.innerText = currentDog.name;
+            dogImage.src = currentDog.url;
             dogImage.onload = function () {
                 dogImage.removeAttribute('aria-hidden');
                 dogLoading.setAttribute('aria-hidden', true);
             };
 
             var dogClickCount = this.dogClickCount;
-            dogClickCount.innerText = selectedDog.clickCount + ' clicks';
-
-            this.eventListener = function () {
-                dogClickCount.innerText = ++selectedDog.clickCount + ' clicks';
-            };
-
-            dogImage.addEventListener('click', this.eventListener);
+            dogClickCount.innerText = currentDog.clickCount + ' clicks';
         }
     };
 
