aa567d0b098f42a89d33010d0da41e26dfc2eb51
Rubens Armôa Lopes <rubens.lopes@gmail.com>
2016-12-18 23:10:09 -0300

    Add support to a dog list

--- js/app.js
+++ js/app.js
@@ -1,9 +1,41 @@
 (function () {
-    var counterValue = 0,
-        counter = document.getElementById('click-counter'),    
-        dog = document.getElementById('dog');
-    
-    dog.addEventListener('click', function () {
-        counter.innerText = ++counterValue;
-    })
+    var dogList = [
+        new Dog('Baby Bilbo', '/images/dog.png'),
+        new Dog('Happy Bilbo', '/images/dog-2.png'),
+    ];
+
+    function Dog(name, url) {
+        this.name = name;
+        this.url = url;
+        this.clickCounter = 0;
+    };
+
+    var dogListElement = document.getElementsByClassName('dog-list')[0];
+
+    dogList.forEach(function (e) {
+        var dogListItem = document.createElement('li');
+        dogListItem.classList.add('dog-list__item');
+        dogListElement.appendChild(dogListItem);
+        
+        var dogName = document.createElement('h2');
+        dogName.innerText = e.name;
+        dogListItem.appendChild(dogName);
+
+        var clickCounter = document.createElement('p');
+        clickCounter.classList.add('dog-list__click-counter');
+        clickCounter.innerText = e.clickCounter;
+        
+        var img = new Image();
+        img.src = e.url;
+        img.classList.add('dog-list__dog');
+        img.alt = 'A dog';
+        img.setAttribute('width', 540);
+        img.setAttribute('height', 540);
+        img.addEventListener('click', function () {
+            clickCounter.innerText = ++e.clickCounter;
+        });
+
+        dogListItem.appendChild(img);
+        dogListItem.appendChild(clickCounter);
+    });
 })();
